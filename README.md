f0a0b75ff5c7aedb15da2f6d4e1ea1e9098de87c
Sergey Moryakov <sergey@moryakov.com>
2023-09-05 13:48:51 +0300

    simple custom ID generator

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # id-generator
+
 An ID generator and timestamp converter.
+
+## Generate Unique ID
+
+Generate a unique random ID string.
+Reference: https://www.npmjs.com/package/generate-unique-id
+
+### Installation
+
+<code>npm i generate-unique-id</code>
+
+### Usage
+
+<code>
+const generateUniqueId = require('generate-unique-id');
+
+// example 1
+const id1 = generateUniqueId();
+
+// example 2
+const id2 = generateUniqueId({
+length: 32,
+useLetters: false
+});
+
+// example 3
+const id3 = generateUniqueId({
+includeSymbols: ['@','#','|'],
+excludeSymbols: ['0']
+});
+</code>
+
+### Options
+
+Option Type Default Description
+length number 20 Length of the generated ID.
+useLetters boolean true Use letters (English alphabet) as part of the generated ID.
+useNumbers boolean true Use numbers as part of the generated ID.
+includeSymbols array [] Use additional letters as part of the generated ID.
+excludeSymbols array [] Do not use these symbols as part of the generated ID.
+
+### License
+
+MIT license
--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("Test message: script link success.");
+const DEFAULT_LENGHT = 12;
+
+const input1Node = document.getElementById("input1");
+const id1Node = document.getElementById("id1");
+const id1BtnNode = document.getElementById("id1Btn");
+
+function generateID(length) {
+    return [...Array(length)]
+        .map(() => (~~(Math.random() * 36)).toString(36))
+        .join("");
+}
+
+// Initial rendering
+input1Node.value = DEFAULT_LENGHT;
+const id1 = generateID(DEFAULT_LENGHT); // Produces a 10-character unique ID
+console.log("ID1: ", id1);
+id1Node.innerText = id1;
+console.log("id1Node.value: ", input1Node.value); // Produces a 10-character unique ID
+
+id1BtnNode.addEventListener("click", () => {
+    id1Node.innerText = "";
+    const newLenght = parseInt(input1Node.value);
+    console.log("newLenght: ", newLenght);
+    const newId1 = generateID(newLenght);
+    id1Node.innerText = newId1;
+});
+
+// import { v4 as uuidv4 } from "./node_modules/uuid";
+
+// console.log(uuidv4());
+
+// const generateUniqueId = require("generate-unique-id");
+// const id1 = generateUniqueId();
+// const id2 = generateUniqueId({
+//     length: 8,
+//     useLetters: false,
+// });
+// const id3 = generateUniqueId({
+//     includeSymbols: ["@", "$", "#"],
+//     excludeSymbols: ["Z", "z"],
+// });
+
+// console.log(`New ID1: ${id1}.`);
+// console.log(`New ID2: ${id2}.`);
+// console.log(`New ID3: ${id3}.`);
